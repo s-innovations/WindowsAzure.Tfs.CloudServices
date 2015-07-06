b08b540873d9e55df4d9a12596ac29b350fdc9ff
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-07-07 01:03:18 +0200

    added a option to update a local resource size

---
+++ WindowsAzure.Tfs.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+
+namespace WindowsAzure.Tfs.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var text = "ExtraSmall";
+            var doc = XDocument.Load("testFile.xml");
+            XNamespace ab = "http://schemas.microsoft.com/ServiceHosting/2008/10/ServiceDefinition";
+            foreach (var element in doc.Root.Elements(ab + "WebRole"))
+            {
+                element.SetAttributeValue("vmsize", text);
+            }
+            foreach (var element in doc.Root.Elements(ab + "WorkerRole"))
+            {
+                FixSize(ab, element, "AscendLocalStorage", "1020");
+            }
+         
+            //  doc.Root.Element(ab + "WebRole").SetAttributeValue("vmsize", text);
+
+           // doc.Save(filePath);
+        }
+        public void FixSize(XNamespace ab, XElement role, string name, string size)
+        {
+            foreach (var local in role.Elements(ab + "LocalResources"))
+                foreach (var storage in local.Elements(ab + "LocalStorage"))
+                    if (storage.Attribute("name").Value == name)
+                        storage.SetAttributeValue("sizeInMB", size);
+        }
+    }
+}
